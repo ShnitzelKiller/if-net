ff6030e5b65a44e6d6735be76694a6e07ccea014
James Noeckel <jamesn8@cs.washington.edu>
2022-08-31 14:29:04 -0700

    made data processing script more flexible

--- data_processing/voxelize.py
+++ data_processing/voxelize.py
@@ -4,21 +4,32 @@ import os
 import glob
 import multiprocessing as mp
 from multiprocessing import Pool
-from functools import partial
+from functools import partial, reduce
 import traceback
 import voxels
 import argparse
+import pathlib
+import operator
 
 
-def voxelize(in_path, res):
+def voxelize(in_path, res, outpath=None):
     try:
-
-        filename = os.path.join(in_path, 'voxelization_{}.npy'.format(res))
+        if os.path.isfile(in_path):
+            infilename = os.path.splitext(os.path.split(in_path)[1])[0]
+        else:
+            infilename = 'voxelization'
+        
+        if outpath is None:
+            outpath = in_path
+        filename = os.path.join(outpath, f'{infilename}_{res}.npy'.format(res))
 
         if os.path.exists(filename):
             return
 
-        mesh = trimesh.load(in_path + '/isosurf_scaled.off', process=False)
+        if os.path.isdir(in_path):
+            in_path = os.path.join(in_path, '/isosurf_scaled.off')
+
+        mesh = trimesh.load(in_path, process=False)
         occupancies = voxels.VoxelGrid.from_mesh(mesh, res, loc=[0, 0, 0], scale=1).data
         occupancies = np.reshape(occupancies, -1)
 
@@ -38,11 +49,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Run voxalization'
     )
-    parser.add_argument('-res', type=int)
+    parser.add_argument('--res', type=int)
+    parser.add_argument('--root', type=str, default='shapenet/data')
+    parser.add_argument('--depth',type=int, default=3)
+    parser.add_argument('--outpath',type=str, default=None)
 
     args = parser.parse_args()
 
-    ROOT = 'shapenet/data'
+    ROOT = pathlib.Path(args.root)
+
+    path = pathlib.Path('*')
+    path = reduce(operator.truediv, (path for _ in range(args.depth)))
 
     p = Pool(mp.cpu_count())
-    p.map(partial(voxelize, res=args.res), glob.glob( ROOT + '/*/*/'))
+    p.map(partial(voxelize, res=args.res, outpath=args.outpath), glob.glob( str(ROOT / path)))
